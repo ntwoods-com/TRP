13dd02ddf74b8c9e58469540c661c7dcfd656513
Rajesh Jadoun <deo01@ntwoods.com>
2025-12-12 17:02:54 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,14 +1,18 @@
 /*************************************************
   NT Woods HRMS - auth.js
-  - Google Login callback
 **************************************************/
+
+function decodeJwt(token) {
+  return JSON.parse(atob(token.split(".")[1]));
+}
 
 async function handleGoogleLogin(response) {
   const user = decodeJwt(response.credential);
 
-  const backend = await fetch(
+  const res = await fetch(
     GAS_WEB_URL + "?action=login&email=" + encodeURIComponent(user.email)
-  ).then(r => r.json());
+  );
+  const backend = await res.json();
 
   if (!backend.success) {
     alert("Login failed: " + (backend.error || "You are not authorized"));
@@ -18,8 +22,4 @@ async function handleGoogleLogin(response) {
 
   localStorage.setItem("hrmsUser", JSON.stringify(backend.user));
   window.location.href = "dashboard.html";
-}
-
-function decodeJwt(token) {
-  return JSON.parse(atob(token.split('.')[1]));
 }
